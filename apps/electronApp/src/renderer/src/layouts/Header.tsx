f33b602acd48272f5de3841b9ef4f6b82cc8c5b5
jo4086 <ghzktm5892@gmail.com>
2025-12-18 00:46:19 +0900

    style(layouts): 레이아웃 스타일 수정

--- apps/electronApp/src/renderer/src/layouts/Header.tsx
+++ apps/electronApp/src/renderer/src/layouts/Header.tsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <>
-      <div className="bg-brand-light">
-        <Link className="" to={{ pathname: '' }}>
-          로고에요
+      <div className="flex justify-center items-center bg-brand-light py-2">
+        <Link className="px-4 py-2 border border-white rounded-md min-w-30 text-center" to={{ pathname: '' }}>
+          로고
         </Link>
       </div>
     </>
